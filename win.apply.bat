43f9d34c47c061d681ce61acf9923797ef4c3d09
Frosty-J <60154347+Frosty-J@users.noreply.github.com>
2023-11-29 19:40:08 +0000

    Revise Windows instructions

--- win.apply.bat
+++
@@ -1,5 +0,0 @@
-copy .\plug-in-file-vera-bitmap.ui C:\Program Files\GIMP 2\share\gimp\2.0\ui\plug-ins\
-copy .\plug-in-file-vera-selector.ui C:\Program Files\GIMP 2\share\gimp\2.0\ui\plug-ins\
-copy .\plug-in-file-vera-tiles.ui C:\Program Files\GIMP 2\share\gimp\2.0\ui\plug-ins\
-mkdir C:\Program Files\GIMP 2\lib\gimp\2.0\plug-ins\vera_tileset
-copy .\vera_tileset.exe C:\Program Files\GIMP 2\lib\gimp\2.0\plug-ins\vera_tileset\
--- win.lxml2fix.bat
+++
@@ -1,1 +0,0 @@
-mklink /D C:\msys64\mingw64\include\libxml C:\msys64\mingw64\include\libxml2\libxml
